5e939fc1de140d1dd83a3ed09ae327b1df1fc1fa
Marcelo Meirelles <meirellesm@hotmail.com>
2024-05-08 22:15:17 -0300

    feat: add lombok to project

--- Week 2/superpecas/src/main/java/br/com/masterclass/superpecas/model/Car.java
+++ Week 2/superpecas/src/main/java/br/com/masterclass/superpecas/model/Car.java
@@ -1,23 +1,31 @@
 package br.com.masterclass.superpecas.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "carro")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Car {
 
     @Id
     @Column(nullable = false)
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
-    Integer id;
+    private Integer id;
 
     @Column(nullable = false)
-    String modelName;
+    private String modelName;
 
     @Column(nullable = false)
-    String manufacturer;
+    private String manufacturer;
 
     @Column(nullable = false, unique = true)
-    String uniqueCode;
+    private String uniqueCode;
 
 }
