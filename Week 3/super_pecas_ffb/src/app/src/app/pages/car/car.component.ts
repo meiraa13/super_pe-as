c82f33114c75dd7871e754e9baea8343522e183b
Marcelo Meirelles <meirellesm@hotmail.com>
2024-05-23 17:00:33 -0300

    feat: add car-table component

--- Week 3/super_pecas_ffb/src/app/src/app/pages/car/car.component.ts
+++ Week 3/super_pecas_ffb/src/app/src/app/pages/car/car.component.ts
@@ -1,37 +1,11 @@
-import { Component, ViewChild, AfterViewInit} from '@angular/core';
+import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button'
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {FormsModule} from '@angular/forms';
 import {MatIconModule} from '@angular/material/icon';
-import {MatTableDataSource, MatTableModule} from '@angular/material/table';
-import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
+import { CarTableComponent } from '../../components/car-table/car-table.component';
 
-export interface ITable {
-  id: number;
-  name: string;
-  manufacturer: string;
-  code: string;
-}
-
-const ELEMENT_DATA: ITable[] = [
-  {id: 1, name: 'Civic', manufacturer: 'Honda', code: 'HJQQER424'},
-  {id: 2, name: 'A4', manufacturer: 'Audi', code: 'HJQQfaer4'},
-  {id: 3, name: 'Polo', manufacturer: 'Volkswagen', code: '1RFQER424'},
-  {id: 3, name: 'Polo', manufacturer: 'Volkswagen', code: '1RFQER424'},
-  {id: 3, name: 'Polo', manufacturer: 'Volkswagen', code: '1RFQER424'},
-  {id: 3, name: 'Polo', manufacturer: 'Volkswagen', code: '1RFQER424'},
-  {id: 3, name: 'Polo', manufacturer: 'Volkswagen', code: '1RFQER424'},
-  {id: 3, name: 'Polo', manufacturer: 'Volkswagen', code: '1RFQER424'},
-  {id: 3, name: 'Polo', manufacturer: 'Volkswagen', code: '1RFQER424'},
-  {id: 3, name: 'Polo', manufacturer: 'Volkswagen', code: '1RFQER424'},
-  {id: 3, name: 'Polo', manufacturer: 'Volkswagen', code: '1RFQER424'},
-  {id: 3, name: 'Polo', manufacturer: 'Volkswagen', code: '1RFQER424'},
-  {id: 3, name: 'Polo', manufacturer: 'Volkswagen', code: '1RFQER424'},
-
-
-
-];
 
 
 @Component({
@@ -43,21 +17,12 @@ const ELEMENT_DATA: ITable[] = [
     MatInputModule,
     FormsModule,
     MatIconModule,
-    MatTableModule,
-    MatPaginatorModule
+    CarTableComponent
   ],
   templateUrl: './car.component.html',
   styleUrl: './car.component.css'
 })
-export class CarComponent implements AfterViewInit {
-  displayedColumns: string[] = ['id', 'name', 'manufacturer', 'code', 'actions'];
-  dataSource = new MatTableDataSource<ITable>(ELEMENT_DATA);
-
-  @ViewChild(MatPaginator) paginator!: MatPaginator;
-
-  ngAfterViewInit(): void {
-    this.dataSource.paginator = this.paginator
-  }
+export class CarComponent {
 
 
 
