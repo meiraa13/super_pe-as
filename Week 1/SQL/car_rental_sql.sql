92af8ffc2af195a008b324465faf468196b835e9
Marcelo Meirelles <meirellesm@hotmail.com>
2024-07-04 20:14:22 -0300

    feat: add new sql table and diagram

---
+++ Week 1/SQL/car_rental_sql.sql
@@ -0,0 +1,31 @@
+CREATE TABLE cars (
+  id varchar(255) PRIMARY KEY,
+  name varchar(255) NOT NULL,
+  manufacturer varchar(255) NOT NULL,
+  year int NOT NULL,
+  color varchar(255) NOT NULL,
+  mileage int NOT NULL,
+  photo varchar(255) UNIQUE NOT NULL,
+  available bool DEFAULT true
+);
+
+CREATE TABLE rentals (
+  id varchar(255) PRIMARY KEY,
+  user varchar(255) UNIQUE NOT NULL,
+  car varchar(255),
+  rentDate datetime NOT NULL,
+  returnDate datetime,
+  initialMileage int NOT NULL,
+  returnMileage int
+  FOREIGN KEY (user) REFERENCES users(id)
+  FOREIGN KEY (car) REFERENCES cars(id)
+);
+
+CREATE TABLE users (
+  id varchar(255) PRIMARY KEY,
+  name varchar(255) NOT NULL,
+  email varchar(255) UNIQUE NOT NULL,
+  cpf varchar(255) NOT NULL,
+  isAdmin bool DEFAULT false
+);
+
